ea06f19a8ad07d5eec7b01d10b44a1f9f3db2592
Jon <jono330@hotmail.com>
2011-05-16 22:56:23 -0400

    everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+
+  before_filter :authenticate, :except => [:show, :new, :create]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  before_filter :anti_authenticate, :only => [:new, :create]
+  
+  def index
+	@title = "All Users"
+	@users = User.paginate(:page => params[:page])
+  end
+  
   def new
 	@user = User.new
  	@title = "Sign Up"
@@ -6,6 +17,7 @@ class UsersController < ApplicationController
   
   def show
 	@user = User.find(params[:id])
+	@microposts = @user.microposts.paginate(:page => params[:page])
   	@title = @user.name
   end
   
@@ -19,6 +31,65 @@ class UsersController < ApplicationController
 		@title = "Sign Up"
 		@user.password = ""
 		render 'new'
-	end	
-  end  
+	end
+  end
+  
+  def edit
+	@title = "Edit User"
+  end
+  
+  def update
+	@user = User.find(params[:id])
+	if @user.update_attributes(params[:user])
+		flash[:success] = "Profile Updated"
+		redirect_to @user
+	else
+		@title = "Edit User"
+		render 'edit'
+	end
+  end
+  
+  def destroy
+	user_tbd = User.find(params[:id])
+	
+	if current_user?(user_tbd)
+		redirect_to(users_path)
+		flash[:notice] = "You cant delete yourself"
+	else
+		user_tbd.destroy
+		flash[:success] = "User Deleted"
+		redirect_to(users_path)
+	end
+  end
+  
+  def following
+	@title = "Following"
+	@user = User.find(params[:id])
+	@users = @user.following.paginate(:page => params[:page])
+	render 'show_follow'
+  end
+  
+  def followers
+	@title = "Followers"
+	@user = User.find(params[:id])
+	@users = @user.followers.paginate(:page => params[:page])
+	render 'show_follow'
+  end
+  
+  private
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+	
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
+	end
+	
+	def anti_authenticate
+		if signed_in?
+			redirect_to(root_path)
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,15 @@ class User < ActiveRecord::Base
 	#regular accessors
 	attr_accessible :name, :email, :password, :password_confirmation
 	
+	
+	has_many :microposts, :dependent => :destroy
+	has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+	
+	has_many :reverse_relationships, :foreign_key => "followed_id", :class_name => "Relationship", :dependent => :destroy
+	has_many :followers, :through => :reverse_relationships, :source => :follower
+	
+	has_many :following, :through => :relationships, :source => :followed
+	
 	#uses a regular expression for email validation
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
@@ -53,6 +62,22 @@ class User < ActiveRecord::Base
 		elsif user.salt == cookie_salt
 			return user
 		end
+	end
+	
+	def feed
+		Micropost.from_users_followed_by(self)
+	end	
+	
+	def following?(followed)
+		relationships.find_by_followed_id(followed)
+	end
+	
+	def follow!(followed)
+		relationships.create!(:followed_id => followed.id)
+	end
+	
+	def unfollow!(followed)
+		relationships.find_by_followed_id(followed).destroy
 	end
 	
 	private
